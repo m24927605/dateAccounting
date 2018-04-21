320bfae1eff822234e84dcd048d6758365006fd1
Michael <m24927605@gmail.com>
2018-04-21 17:47:44 +0800

    add line config

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@
 module.exports = (app) => {
     app.get('/', (req, res) => {
         let title = process.env.NODE_ENV;
-        res.render('index.html', { title: title });
+        let channelID = process.env.CHANNEL_ID;
+        let channelSecret = process.env.CHANNEL_SECRET;
+        let channelToken = process.env.CHANNEL_ACCESS_TOKEN;
+        res.render('index.html', { title: title,channelID:channelID,channelSecret:channelSecret,channelToken:channelToken });
     });
     app.get('/index', (req, res) => {
         let title = process.env.NODE_ENV;
